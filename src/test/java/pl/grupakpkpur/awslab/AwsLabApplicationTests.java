e094175514f827c1872869bcd17d6261200c9605
Kamil Przeniosło <kamillo.prz@gmail.com>
2021-10-15 20:48:51 +0200

    Initial commit

---
+++ src/test/java/pl/grupakpkpur/awslab/AwsLabApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.grupakpkpur.awslab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
