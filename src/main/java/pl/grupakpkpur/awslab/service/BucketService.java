62f47a427423404113abfe47d8a69e64046d362e
Kamil Przeniosło <kamillo.prz@gmail.com>
2021-10-16 00:26:57 +0200

    Add listing s3 objects

--- src/main/java/pl/grupakpkpur/awslab/service/BucketService.java
+++ src/main/java/pl/grupakpkpur/awslab/service/BucketService.java
@@ -3,8 +3,10 @@ package pl.grupakpkpur.awslab.service;
 import java.util.List;
 import org.springframework.stereotype.Service;
 import pl.grupakpkpur.awslab.model.BucketDto;
+import pl.grupakpkpur.awslab.model.S3ObjectDto;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
 
 @Service
 public class BucketService {
@@ -14,6 +16,13 @@ public class BucketService {
   public List<BucketDto> listBuckets() {
     return s3.listBuckets().buckets().stream()
         .map(bucket -> new BucketDto(bucket.name(), bucket.creationDate()))
+        .toList();
+  }
+
+  public List<S3ObjectDto> listFilesInBucket(String bucketName) {
+    ListObjectsV2Request request = ListObjectsV2Request.builder().bucket(bucketName).build();
+    return s3.listObjectsV2(request).contents().stream()
+        .map(s3Object -> new S3ObjectDto(s3Object.key(), s3Object.size(), s3Object.lastModified()))
         .toList();
   }
 }
