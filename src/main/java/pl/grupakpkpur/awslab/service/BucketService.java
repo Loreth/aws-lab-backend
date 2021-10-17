40e9b8686e63f249a9b32f033e49f4910ce2c23e
Urszula Mazur <ulam98@gmail.com>
2021-10-18 00:07:29 +0200

    Added functionality for archiving files in buckets

--- src/main/java/pl/grupakpkpur/awslab/service/BucketService.java
+++ src/main/java/pl/grupakpkpur/awslab/service/BucketService.java
@@ -1,11 +1,16 @@
 package pl.grupakpkpur.awslab.service;
 
-import java.util.List;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 import pl.grupakpkpur.awslab.model.BucketDto;
 import pl.grupakpkpur.awslab.model.S3ObjectDto;
+import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.IOException;
+import java.util.List;
 
 @Service
 public class BucketService {
@@ -22,5 +27,20 @@ public class BucketService {
     return s3.listObjectsV2(builder -> builder.bucket(bucketName)).contents().stream()
         .map(s3Object -> new S3ObjectDto(s3Object.key(), s3Object.size(), s3Object.lastModified()))
         .toList();
+  }
+
+  public void uploadFilesToBucket(String bucketName, List<MultipartFile> files) {
+    for (MultipartFile f:files) {
+    PutObjectRequest objectRequest = PutObjectRequest.builder()
+            .bucket(bucketName)
+            .key(f.getOriginalFilename())
+            .build();
+
+      try {
+        s3.putObject(objectRequest, RequestBody.fromBytes(f.getBytes()));
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
   }
 }
