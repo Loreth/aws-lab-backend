1b570ebfca1ae118f40ac76777c4812350eda10a
Kamil Przeniosło <kamillo.prz@gmail.com>
2021-10-17 21:08:44 +0200

    Add presigned download url to S3 Objects

--- src/main/java/pl/grupakpkpur/awslab/service/BucketService.java
+++ src/main/java/pl/grupakpkpur/awslab/service/BucketService.java
@@ -1,16 +1,22 @@
 package pl.grupakpkpur.awslab.service;
 
+import java.net.URL;
+import java.time.Duration;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import pl.grupakpkpur.awslab.model.BucketDto;
 import pl.grupakpkpur.awslab.model.S3ObjectDto;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Service
+@RequiredArgsConstructor
 public class BucketService {
   private static final Region REGION = Region.US_EAST_1;
   private final S3Client s3 = S3Client.builder().region(REGION).build();
+  private final S3Presigner s3Presigner;
 
   public List<BucketDto> listBuckets() {
     return s3.listBuckets().buckets().stream()
@@ -20,7 +26,29 @@ public class BucketService {
 
   public List<S3ObjectDto> listFilesInBucket(String bucketName) {
     return s3.listObjectsV2(builder -> builder.bucket(bucketName)).contents().stream()
-        .map(s3Object -> new S3ObjectDto(s3Object.key(), s3Object.size(), s3Object.lastModified()))
+        .map(
+            s3Object ->
+                new S3ObjectDto(
+                    s3Object.key(),
+                    s3Object.size(),
+                    s3Object.lastModified(),
+                    createPresignedS3ObjectDownloadUrl(bucketName, s3Object.key())))
         .toList();
+  }
+
+  private URL createPresignedS3ObjectDownloadUrl(String bucketName, String objectKey) {
+    return s3Presigner
+        .presignGetObject(
+            presignRequestBuilder ->
+                presignRequestBuilder
+                    .signatureDuration(Duration.ofHours(2))
+                    .getObjectRequest(
+                        objectRequestBuilder ->
+                            objectRequestBuilder
+                                .bucket(bucketName)
+                                .key(objectKey)
+                                .responseContentDisposition(
+                                    "attachment; filename=\"" + objectKey + "\"")))
+        .url();
   }
 }
