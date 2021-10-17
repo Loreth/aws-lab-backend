7fe043e1ba577bc1318254e4e8df6ddb6df34c8f
Kamil Plich <kamilplich2@gmail.com>
2021-10-17 18:46:19 +0200

    feat: front-end sketch

--- src/main/java/pl/grupakpkpur/awslab/dto/ec2/Ec2InstanceResponse.java
+++ src/main/java/pl/grupakpkpur/awslab/dto/ec2/Ec2InstanceResponse.java
@@ -3,14 +3,24 @@ package pl.grupakpkpur.awslab.dto.ec2;
 import software.amazon.awssdk.services.ec2.model.Instance;
 import software.amazon.awssdk.services.ec2.model.InstanceStateName;
 import software.amazon.awssdk.services.ec2.model.InstanceType;
+import software.amazon.awssdk.services.ec2.model.Tag;
 
-public record Ec2InstanceResponse(String instanceId, String imageId, InstanceType instanceType, InstanceStateName instanceState) {
+public record Ec2InstanceResponse(String id, String name, String imageId, InstanceType type, InstanceStateName state) {
 
 	public static Ec2InstanceResponse fromInstance(Instance instance) {
 		return new Ec2InstanceResponse(instance);
 	}
 
 	private Ec2InstanceResponse(Instance instance) {
-		this(instance.instanceId(), instance.imageId(), instance.instanceType(), instance.state().name());
+		this(
+				instance.instanceId(),
+				instance.tags().stream()
+						.filter(tag -> tag.key().equals("Name"))
+						.findFirst()
+						.map(Tag::key)
+						.orElse(null),
+				instance.imageId(),
+				instance.instanceType(),
+				instance.state().name());
 	}
 }
