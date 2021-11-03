d6cba1e8b9d346208881cc3a15b9b5ebb88af821
Urszula Mazur <ulam98@gmail.com>
2021-11-03 19:33:31 +0100

    added methods to get tags from db

--- src/main/java/pl/grupakpkpur/awslab/repository/LoggingRepository.java
+++ src/main/java/pl/grupakpkpur/awslab/repository/LoggingRepository.java
@@ -4,8 +4,14 @@ import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.CrudRepository;
 import pl.grupakpkpur.awslab.model.logging.LogEntry;
 
+import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 @EnableScan
 public interface LoggingRepository extends CrudRepository<LogEntry, UUID> {
+    List<LogEntry> findAllByUsername(String username);
+    List<LogEntry> findAllByDateBetween(Date start, Date end);
+    List<LogEntry> findAllByDateBetweenAndUsername(Date start, Date end, String username);
+
 }
