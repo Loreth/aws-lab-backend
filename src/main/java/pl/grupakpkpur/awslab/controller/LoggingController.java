25051a562506e8ad9d901d248435547f102dc2a7
Urszula Mazur <ulam98@gmail.com>
2021-11-03 19:33:03 +0100

    Added methods to get all tags form db, or tags filtered by dates or/and user surname

--- src/main/java/pl/grupakpkpur/awslab/controller/LoggingController.java
+++ src/main/java/pl/grupakpkpur/awslab/controller/LoggingController.java
@@ -5,19 +5,43 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import pl.grupakpkpur.awslab.model.authentication.User;
+import pl.grupakpkpur.awslab.model.logging.LogDatesRequest;
 import pl.grupakpkpur.awslab.model.logging.LogEntryRequest;
+import pl.grupakpkpur.awslab.model.logging.LogEntryResponse;
 import pl.grupakpkpur.awslab.service.LoggingService;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("${rest.mapping.logging}")
 public class LoggingController {
 
-	private final LoggingService loggingService;
+    private final LoggingService loggingService;
 
-	@PostMapping("/log-entry")
-	public void log(Authentication authentication, @RequestBody LogEntryRequest request) {
-		User user = (User) authentication.getPrincipal();
-		this.loggingService.logEntry(request, user);
-	}
+    @PostMapping("/log-entry")
+    public void log(Authentication authentication, @RequestBody LogEntryRequest request) {
+        User user = (User) authentication.getPrincipal();
+        this.loggingService.logEntry(request, user);
+    }
+
+    @PostMapping("/dates")
+    public List<LogEntryResponse> getLogsByDate(@RequestBody LogDatesRequest dates) {
+        return loggingService.getLogsByDate(dates.start(), dates.end());
+    }
+
+    @PostMapping("/dates/{username}")
+    public List<LogEntryResponse> getLogsByDateAndUsername(@PathVariable String username, @RequestBody LogDatesRequest dates) {
+        return loggingService.getLogsByDateAndUsername(dates.start(), dates.end(), username);
+    }
+
+    @GetMapping("/logs")
+    public List<LogEntryResponse> getLogs() {
+        return loggingService.getLogs();
+    }
+
+    @GetMapping("/user/{username}")
+    public List<LogEntryResponse> getLogsByUsername(@PathVariable String username) {
+        return loggingService.getLogsByUsername(username);
+    }
 }
